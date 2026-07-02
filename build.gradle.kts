eb20f1441db937b99258084dda65d546be9fdd75
huyvipp <118251131+huyvipp@users.noreply.github.com>
2026-07-02 22:58:33 +0700

    Refactor versioning to use environment variables
    Removed versioning functions and replaced with environment variables for git version and builder.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,10 @@
-import java.io.ByteArrayOutputStream
-
 plugins {
     id("java")
     id("com.gradleup.shadow") version "9.3.0"
 }
 
-val git: String = versionBanner()
-val builder: String = builder()
-
-ext["git_version"] = git
-ext["builder"] = builder
+ext["git_version"] = "Unknown"
+ext["builder"] = System.getenv("GITHUB_ACTOR") ?: "Unknown"
 
 subprojects {
     apply(plugin = "java")
@@ -28,30 +23,5 @@ subprojects {
                 Pair("config_version", rootProject.properties["config_version"]!!)
             )
         }
-    }
-}
-
-fun versionBanner(): String {
-    return runGit("rev-parse", "--short=8", "HEAD")
-}
-
-fun builder(): String {
-    return runGit("config", "user.name")
-}
-
-fun runGit(vararg args: String): String {
-    return try {
-        val output = ByteArrayOutputStream()
-
-        project.exec {
-            commandLine("git", *args)
-            standardOutput = output
-            errorOutput = ByteArrayOutputStream()
-            isIgnoreExitValue = true
-        }
-
-        output.toString().trim().ifBlank { "Unknown" }
-    } catch (_: Exception) {
-        "Unknown"
     }
 }
